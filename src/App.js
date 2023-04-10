a7f5f81123982e63b35c494a59144a92726a96c2
Dwinatech <diwina1993@hotmail.com>
2023-04-10 03:21:34 +0100

    Variables lesson

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import "./App.css";
 
 const App = () => {
+  const tutorialName = "JavaScript";
+
+  // const max = 10;
+  // const min = 0;
+  // const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+
+  // let tutorialName = "JavaScript";
+  // if (randomNumber % 2 === 0) {
+  //   tutorialName = "HTML";
+  // }
+
   return (
     <div className="App">
-      <h1>JavaScript</h1>
+      <h1>{tutorialName}</h1>
+      {/* <h2>Random Number: {randomNumber}</h2> */}
     </div>
   );
 };
