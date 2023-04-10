e2cf2092ec514c0792fec290b953e7df12eb08c7
Dwinatech <diwina1993@hotmail.com>
2023-04-10 16:16:43 +0100

    Comparison Operators

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import "./App.css";
 
 const App = () => {
+  const age = 58;
+  let message = "";
+
+  if (age === 55) {
+    message = "Mohammed is 55 years old";
+  } else if (age < 50) {
+    message = "Mohammed is younger then 50 years old";
+  } else if (age > 60) {
+    message = "Mohammed is older then 60 years old";
+  } else if (age <= 54) {
+    message = "Mohammed age is 54 years old or less";
+  } else if (age >= 59) {
+    message = "Mohammed age is 59 years old or more";
+  } else if (age !== 20) {
+    // 58 - 56
+    message = `Mohammed age is not equal to 20 years, it is ${age}`;
+  } else {
+    message = "";
+  }
+
   return (
     <div className="App">
-      <h1>JavaScript</h1>
+      <h1>JavaScript tutorial, Arithmetic Operators:</h1>
+      <p>{message}</p>
     </div>
   );
 };
