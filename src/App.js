8300fe1a0c7ed519a6ceb7e6ccb7a019b8fa57d6
Dwinatech <diwina1993@hotmail.com>
2023-04-10 15:53:38 +0100

    Arithmetic Operators

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import "./App.css";
 
 const App = () => {
+  let five = 5;
+  let ten = 10;
+
   return (
     <div className="App">
-      <h1>JavaScript</h1>
+      <h1>JavaScript tutorial, Arithmetic Operators:</h1>
+      <h2>five + ten: {five + ten}</h2>
+      <h2>five - ten: {five - ten}</h2>
+      <h2>five * ten: {five * ten}</h2>
+      <h2>five / ten: {five / ten}</h2>
+      <h2>Increment five: {--five}</h2>
+      <h2>Decrement five: {++five}</h2>
     </div>
   );
 };
