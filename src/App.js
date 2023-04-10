e0d79c2f8f647a20b435171508489c358a0f8477
Dwinatech <diwina1993@hotmail.com>
2023-04-10 03:36:08 +0100

    Condition in JavaScript

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import "./App.css";
 
 const App = () => {
+  const max = 10;
+  const min = 0;
+  const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+
+  let tutorialName = "JavaScript";
+  if (randomNumber % 2 === 0) {
+    tutorialName = "HTML";
+  }
+
+  // const tutorialName = randomNumber % 2 === 0 ? tutorialName = "HTML" : "JavaScript";
+
   return (
     <div className="App">
-      <h1>JavaScript</h1>
+      <h1>{tutorialName}</h1>
+      <h2>Random Number: {randomNumber}</h2>
     </div>
   );
 };
