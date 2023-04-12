668db7c52a626ff1a3afef2b1b20d29da8ba163d
Dwinatech <diwina1993@hotmail.com>
2023-04-12 13:30:18 +0100

    update

--- src/App.js
+++ src/App.js
@@ -7,12 +7,12 @@ const App = () => {
   return (
     <div className="App">
       <h1>JavaScript tutorial, Arithmetic Operators:</h1>
-      <h2>five + ten: {five + ten}</h2>
-      <h2>five - ten: {five - ten}</h2>
-      <h2>five * ten: {five * ten}</h2>
-      <h2>five / ten: {five / ten}</h2>
-      <h2>Increment five: {--five}</h2>
-      <h2>Decrement five: {++five}</h2>
+      <h2>five + ten = {five + ten}</h2>
+      <h2>five - ten = {five - ten}</h2>
+      <h2>five * ten = {five * ten}</h2>
+      <h2>five / ten = {five / ten}</h2>
+      <h2>Increment five = {--five}</h2>
+      <h2>Decrement five = {++five}</h2>
     </div>
   );
 };
