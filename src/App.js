088ee1b4246d94361104d603b423e63786c89b7a
Dwinatech <diwina1993@hotmail.com>
2023-04-16 13:12:44 +0100

    Events lesson

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import "./App.css";
 
 const App = () => {
+  let defaultValue = "";
+  // Callback function
+  const handleChange = (event) => {
+    defaultValue = event.target.value;
+  };
+
+  // Callback function
+  const handleClick = (event) => {
+    alert(defaultValue);
+  };
+
   return (
     <div className="App">
       <div>
-        <h1>JavaScript</h1>
+        <h1>JavaScript events:</h1>
+        <input onChange={handleChange} onMouseDown={() => {}} />
+        <button onClick={handleClick}>Submit</button>
       </div>
     </div>
   );
