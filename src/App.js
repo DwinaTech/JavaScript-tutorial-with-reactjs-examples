3a646a4037201f6009eb6ef2f361c946892f7faa
Dwinatech <diwina1993@hotmail.com>
2023-04-20 16:04:43 +0100

    String Search Methods

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import "./App.css";
 
 const App = () => {
+  const name = "Dwinatech channel";
+  // String.indexOf("characters need to get its index") will return characters
+  // String.lastIndexOf("characters need to get its index") will return characters
+  // String.includes("characters looking for") will return a boolean true or false
+
   return (
     <div className="App">
       <div>
-        <h1>JavaScript</h1>
+        <h1>JavaScript String Search Method</h1>
+        <li>
+          Index of a character: <strong>{name.indexOf("a")}</strong>
+        </li>
+        <li>
+          Index of last character: <strong>{name.lastIndexOf("a")}</strong>
+        </li>
+        <li>
+          Does name includes t character:{" "}
+          <strong>{`${name.includes("t")}`}</strong>
+        </li>
+        <li>
+          Does name start with D character:{" "}
+          <strong>{`${name.startsWith("D")}`}</strong>
+        </li>
+        <li>
+          Does name end with h character:{" "}
+          <strong>{`${name.endsWith("l")}`}</strong>
+        </li>
       </div>
     </div>
   );
